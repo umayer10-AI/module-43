6cfdb4ba9d708ac512e5a1a01b29106f1821c4ec
umayer10-AI <mdumayer10@gmail.com>
2026-04-18 19:46:37 +0600

    add

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 
-const page = () => {
+// const getPosts = async () => {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     return await res.json()
+// }
+
+const getPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    if(!res.ok){
+        throw new Error("Failed to fetch posts")
+    }
+    return await res.json()
+}
+
+// const getPosts = async () => {
+//     try{
+//         const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+//         return await res.json()
+//     }
+//     catch(err){
+//         throw new Error("Failed to fetch posts")
+//     }
+// }
+
+const page = async () => {
+
+    const data = await getPosts()
+    // console.log(data)
+
     return (
         <div>
             Post beta
