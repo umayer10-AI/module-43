7bcddd33b52c7bb9d3738a779b2917a491820afa
umayer10-AI <mdumayer10@gmail.com>
2026-04-18 19:29:18 +0600

    first commit

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return (
-    <div>
-      HEllo
-    </div>
-  );
+    return (
+        <div>
+            Post beta
+        </div>
+    );
 };
 
 export default page;
